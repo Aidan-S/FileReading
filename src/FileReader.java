66afe7545068a19493d6582342f11b145a3313ae
scannella <scannella@bishopireton.org>
2017-09-26 09:50:22 -0400

    Basicaly Done

--- src/FileReader.java
+++ src/FileReader.java
@@ -6,81 +6,86 @@ import java.util.Scanner;
 
 /**
  * 
- */
-
-/**
  * @author scannella
  *
  */
 public class FileReader {
 
 	/**
-	 * @param args
+	 * 
+	 * @param fname
+	 * @param fnum
+	 * @param out
+	 * @return
 	 */
-	public static Scanner openWords(String fname, int fnum) {
-		
+	public static Scanner openWords(String fname, int fnum, PrintWriter out) {
 		File file = new File(fname);
 		Scanner input = null;
 		try {
 			input = new Scanner(file);
 		} catch (FileNotFoundException ex) {
-			System.out.print("Part " + fnum + ": Unable to Open File");
+			out.println("Part " + fnum + ": Unable to Open File");
 			return null;
 		}
-		
-
 		return input;	
 	}
 	
-	
+	/**
+	 * 
+	 * @param fName
+	 * @return
+	 */
 	public static PrintWriter outputFile(String fName) {
 			File fileName = new File(fName);
-
 			PrintWriter output = null;
-
 			try {
 				output = new PrintWriter(fileName);
 			} catch (FileNotFoundException ex) {
 				System.out.print("Cannot open " + fName);
 				return null;
-
 			}
-
 			return output;
 	}
 	
-	
+	/**
+	 * 
+	 * @param fname
+	 * @return
+	 */
 	public static boolean braces(Scanner fname) {
 		int braces = 0;
+		char c;
 		while(fname.hasNextLine()){
 			String line = fname.nextLine();
 			for(int i = 0; i < line.length(); i++) {
-				char c = line.charAt(i);
+				c = line.charAt(i);
 				if(c == '{') {
 					braces++;
 				}
 				if(c == '}') {
 					braces--;
 				}	
-					
-			}
-			
+				if(braces < 0) {return false;}
+			}	
 		}
-		
 		if(braces == 0) {
 			return true;
 		}else {
 			return false;
 		}
-	
 	}
 		
-	
+	/**
+	 * 
+	 * @param first
+	 * @param second
+	 * @return
+	 */
 	public static boolean compare(Scanner first, Scanner second) {
 		while (first.hasNextLine() && second.hasNextLine()) {
 			String line1 = first.nextLine();
 			String line2 = second.nextLine();
-			System.out.println("loop");
+			
 
 			if(!(line1.equals(line2))) {return false;}
 			if((first.hasNextLine() && !second.hasNextLine()) || (!first.hasNextLine() && second.hasNextLine())) {return false;}	
@@ -88,22 +93,31 @@ public class FileReader {
 		return true;
 	}
 	
-	
-	public static void storyTime(Scanner story, ArrayList<String> words) {
+	/**
+	 * 
+	 * @param story
+	 * @param out
+	 * @param words
+	 */
+	public static void storyTime(Scanner story, PrintWriter out, ArrayList<String> words) {
 		int i = 0;
-		while (story.hasNextLine()&&i < words.size()) {
+		while (story.hasNextLine() && i < words.size()) {
 			String line = story.nextLine();
 
 			while (line.indexOf('<') > -1 && line.indexOf('>') > -1) {
 				line = line.substring(0, line.indexOf('<')) + words.get(i) + line.substring(line.indexOf('>') + 1);
 				i++;
 			}
-			System.out.println(line);
+			out.println(line);
 		}
 
 	}
 	
-	
+	/**
+	 * 
+	 * @param in
+	 * @return
+	 */
 	public static ArrayList<String> storyWordsSupp(Scanner in) {
 		ArrayList<String> words = new ArrayList<String>();
 		while (in.hasNextLine()) {
@@ -112,7 +126,11 @@ public class FileReader {
 		return words;
 	}
 	
-	
+	/**
+	 * 
+	 * @param file
+	 * @return
+	 */
 	public static ArrayList<String> userInput(Scanner file) {
 		ArrayList<String> words = new ArrayList<String>();
 		Scanner keyboard = new Scanner(System.in);
@@ -130,56 +148,47 @@ public class FileReader {
 			}
 		}
 		file.close();
-		keyboard.close();
 		return words;
 	}
 	
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Scanner file1 = openWords(args[0], 1);
-		Scanner file2 = openWords(args[1], 2);
-		Scanner file3 = openWords(args[2], 3);
 		PrintWriter out = outputFile("output.txt");
+		Scanner file1 = openWords(args[0], 1, out);
 		
 		if (braces(file1)) {
 			out.println("Braces Balanced");
 		}else{
 			out.println("Braces Not Balanced");
 		}
-		
 		out.println();
 		
+		Scanner file2 = openWords(args[1], 2, out);
 		if (compare(file1, file2)) {
 			out.println("Files Identical");
 		}else{
 			out.println("Files Not Identical");
 		}
-		
 		out.println();
 		
-		
+		Scanner file3 = openWords(args[2], 3, out);
 		if (args.length < 4) {
 			ArrayList<String> words = userInput(file3);
-			file3 = openWords(args[2], 3);
-			storyTime(file3, words);
+			file3 = openWords(args[2], 3, out);
+			storyTime(file3, out, words);
 		}else{
-			Scanner file4 = openWords(args[3], 4);
+			Scanner file4 = openWords(args[3], 4, out);
 			ArrayList<String> words = storyWordsSupp(file4);
-			storyTime(file3, words);
+			storyTime(file3, out, words);
+			file4.close();
 		}
-		
 		
 		file1.close();
 		file2.close();
 		file3.close();
-		
-		
-		out.close();
-		System.out.println("Done");
-			
+		out.close();			
 	}
-
-
-	
-
 }
